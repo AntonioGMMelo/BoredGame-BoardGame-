8b2cf1c8769907a9115225979182287fdc2d3b02
AntonioGMMelo <AntonioGMMelo@yahoo.com.br>
2020-10-29 20:56:07 +0000

    MOVE WORKS

--- src/Functions.scala
+++ src/Functions.scala
@@ -6,25 +6,26 @@ object a {
   val BOUNDARY_RIGHT: Int = 1080; //where the center pixel on the right most spaces are
   val BOUNDARY_TOP: Int = 0; //Where the center of the top most spaces are
   val BOUNDARY_BOTTOM: Int = 1080 //Where the center of the bottom most spaces are
-
     @tailrec
   def Move(NOfSpaces: Int, PixelsPerSpace: Int, CharacterPosition: (Int, Int)): Unit = {
-    if (NOfSpaces != 0) {
+    var CharPositionAfter: (Int, Int) =(0,0)
+    if (NOfSpaces > 0) {
       if (BOUNDARY_BOTTOM == CharacterPosition._2 && BOUNDARY_LEFT < CharacterPosition._1) {
-        CharacterPosition._1 - PixelsPerSpace
+        CharPositionAfter = (CharacterPosition._1 - PixelsPerSpace, CharacterPosition._2)
       } else {
         if (BOUNDARY_TOP < CharacterPosition._2 && BOUNDARY_LEFT == CharacterPosition._1) {
-          CharacterPosition._2 - PixelsPerSpace
+          CharPositionAfter = (CharacterPosition._1, CharacterPosition._2 - PixelsPerSpace)
         } else {
           if (BOUNDARY_TOP == CharacterPosition._2 && BOUNDARY_RIGHT > CharacterPosition._1) {
-            CharacterPosition._1 + PixelsPerSpace
+            CharPositionAfter = (CharacterPosition._1 + PixelsPerSpace, CharacterPosition._2)
           } else {
-            CharacterPosition._2 + PixelsPerSpace
+            CharPositionAfter = (CharacterPosition._1, CharacterPosition._2 + PixelsPerSpace)
           }
         }
       }
-     }
-    Move(NOfSpaces - 1, PixelsPerSpace, CharacterPosition)
+      println(CharacterPosition)
+      Move(NOfSpaces - 1, PixelsPerSpace, CharPositionAfter)
+    }
   }
 
   def main(args: Array[String]): Unit = {
