390a6f2d9b701d9888c4c2841a421afe57dc8961
AntonioGMMelo <AntonioGMMelo@yahoo.com.br>
2020-10-30 21:42:01 +0000

    documented move and answered question

--- src/Functions.scala
+++ src/Functions.scala
@@ -9,24 +9,24 @@ object a {
   val BOUNDARY_TOP: Int = 0; //Where the center of the top most spaces are
   val BOUNDARY_BOTTOM: Int = 1080 //Where the center of the bottom most spaces are
     @tailrec
-  def Move(NOfSpaces: Int, PixelsPerSpace: Int, CharacterPosition: (Int, Int)): Unit = {
+  def Move(NOfSpaces: Int, PixelsPerSpace: Int, CharacterPosition: (Int, Int)): Unit = { //Moves the character by PixelsPerSpace NOfSpaces times movement is like a square starts at the bottom right and
     var CharPositionAfter: (Int, Int) =(0,0)
     if (NOfSpaces > 0) {
-      if (BOUNDARY_BOTTOM == CharacterPosition._2 && BOUNDARY_LEFT < CharacterPosition._1) {
+      if (BOUNDARY_BOTTOM == CharacterPosition._2 && BOUNDARY_LEFT < CharacterPosition._1) { //Moves to the left first
         CharPositionAfter = (CharacterPosition._1 - PixelsPerSpace, CharacterPosition._2)
       } else {
-        if (BOUNDARY_TOP < CharacterPosition._2 && BOUNDARY_LEFT == CharacterPosition._1) {
+        if (BOUNDARY_TOP < CharacterPosition._2 && BOUNDARY_LEFT == CharacterPosition._1) {//then up
           CharPositionAfter = (CharacterPosition._1, CharacterPosition._2 - PixelsPerSpace)
         } else {
-          if (BOUNDARY_TOP == CharacterPosition._2 && BOUNDARY_RIGHT > CharacterPosition._1) {
+          if (BOUNDARY_TOP == CharacterPosition._2 && BOUNDARY_RIGHT > CharacterPosition._1) { //then to the right
             CharPositionAfter = (CharacterPosition._1 + PixelsPerSpace, CharacterPosition._2)
           } else {
-            CharPositionAfter = (CharacterPosition._1, CharacterPosition._2 + PixelsPerSpace)
+            CharPositionAfter = (CharacterPosition._1, CharacterPosition._2 + PixelsPerSpace) //then down
           }
         }
       }
       println(CharacterPosition)
-      Move(NOfSpaces - 1, PixelsPerSpace, CharPositionAfter)
+      Move(NOfSpaces - 1, PixelsPerSpace, CharPositionAfter) //recursivity
     }
   }
 }
@@ -35,7 +35,7 @@ object b{
   type Name = String
   type Color = Color
   type Player = (Name, Color)
-  var Players = List[Player] //único problema: listas são imutáveis. talvez utilizar linked lists?
+  var Players = List[Player] //único problema: listas são imutáveis. talvez utilizar linked lists? Podemos "criar" uma lista nova toda vez, não é problema
 
   def CreatePlayer(NewName: String, NewColor: Color): Unit ={
     var NewPlayer: Player = (NewName, NewColor)
