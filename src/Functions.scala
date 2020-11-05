e39dafb252752624e5027565a9f16db7887dcd23
AntonioGMMelo <AntonioGMMelo@yahoo.com.br>
2020-11-05 20:24:01 +0000

    mode move a higher order function

--- src/Functions.scala
+++ src/Functions.scala
@@ -11,9 +11,8 @@ object player {
   private val wheelItems = List("Move Back 1 Space","Move Back 1 Space","Move Back 1 Space","Move Back 2 Spaces","Move Back 2 Spaces","All Players Move Back 2 Spaces","Move Forward 1 Space","Move Forward 1 Space","Move Forward 1 Space","Move Forward 2 Spaces","Move Forward 2 Spaces","All Players Move Forwards 2 Spaces","Go To Jail","Move Forward 3 Spaces","Stay","Roll The dice","Roll The Weighted Dice") //Wheel options
   private val cards = List("Roll The dice","Roll The Weighted Dice","Go To Jail","Get Out Of Jail Free Card","50/50","Skip Question","Dilate Time")//card options
 
-  def move(NOfSpaces: Int, PixelsPerSpace: Int, CharacterPosition: (Int, Int),forwards: Boolean): (Int,Int) = { //move if forwards == true call moveForward else calls moveBackward
-    if(forwards == true) moveForward(NOfSpaces,PixelsPerSpace,CharacterPosition) else moveBackward(NOfSpaces,PixelsPerSpace,CharacterPosition)
-
+  def move(NOfSpaces: Int, PixelsPerSpace: Int, CharacterPosition: (Int, Int),f: (Int,Int,(Int,Int)) => (Int,Int)): (Int,Int) = { //move if forwards == true call moveForward else calls moveBackward
+    f(NOfSpaces, PixelsPerSpace, CharacterPosition)
   }
     @tailrec
   def moveForward(NOfSpaces: Int, PixelsPerSpace: Int, CharacterPosition: (Int, Int)): (Int,Int) = { //Moves the character by PixelsPerSpace NOfSpaces times movement is like a square starts at the bottom right and
@@ -69,8 +68,8 @@ object player {
       dice(r.nextInt(dice.size))
   }
   def coinToss(): Int ={//random number between 0-1 equating to a coin toss 1=Heads, 0=Tails
-    val r = new scala.util.Random
-    r.nextInt(2)
+      val r = new scala.util.Random
+      r.nextInt(2)
   }
   def weightedDice() : Int ={  //a 3 faced dice with the values 2,4 or 6
       val aux =dice.filter(_ <4)  //filters dice to List(1,2,3).
@@ -78,17 +77,17 @@ object player {
       throwDice(diceRoll)
   }
   def spinTheWheel(wheelItems:List[String]): String ={ //returns a String wth the action where the wheel landed
-    val r = new scala.util.Random
+      val r = new scala.util.Random
     wheelItems(r.nextInt(wheelItems.size)) //Makes the wheel spin random with different odds for different actions because of the number of times an action is in the list
   }
   def pullCard(cards:List[String]): String ={ //returns a String wth the action where the wheel landed
-    val r = new scala.util.Random
-    cards(r.nextInt(cards.size)) //Makes the wheel spin random with different odds for different actions because of the number of times an action is in the list
+      val r = new scala.util.Random
+      cards(r.nextInt(cards.size)) //Makes the wheel spin random with different odds for different actions because of the number of times an action is in the list
   }
 }
 
 
-
+/*
 object user{
 
   type Name = String
@@ -125,4 +124,4 @@ object user{
       }
     }
   }
-}
+}*/
