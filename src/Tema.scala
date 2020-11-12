50e907df09756efb3a781aea8ac1ade447037e2f
chico88687 <fvieira9835@gmail.com>
2020-11-12 16:26:25 +0000

    oi

--- src/Tema.scala
+++ src/Tema.scala
@@ -1,35 +1,26 @@
 case class Tema(tema: String, perguntas: List[Pergunta]) {
-//  type tema = tema
-//  type perguntas = perguntas
 
-  def add_Pergunta(pergunta: Pergunta): Unit = {
-    perguntas :+ pergunta
+
+  def add_Pergunta(pergunta: Pergunta) = {
+    Tema.add_Pergunta(pergunta, this)
   }
 }
 
-object Tema{
-
+object Tema {
+  def add_Pergunta(pergunta: Pergunta, tema: Tema): Unit = {
+    
+  }
 }
-
-
 
 case class Pergunta(pergunta: String, alternativas: List[(String, Boolean)]) {
-//  type pergunta = pergunta
-//  type alternativas = alternativas
+  def create_Pergunta() = {
+    Pergunta.create_Pergunta(this)
+  }
+}
 
-  def sort_Alternativas(): Unit = {
+object Pergunta {
+  def create_Pergunta(): Unit = {
 
   }
 
-  def selected_alternativa(alternativa: (String, Boolean)): Boolean = {
-    alternativas match {
-      case x :: t => {
-        if (x == alternativa)
-          if (x._2 == false)
-            false
-          else
-            true
-      }false
-    }
-  }
 }
