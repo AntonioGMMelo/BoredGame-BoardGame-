e59ed64c598f6488ffa32f337269788a46e97fca
chico88687 <fvieira9835@gmail.com>
2020-11-18 13:33:01 +0000

    a

--- src/Tema.scala
+++ src/Tema.scala
@@ -1,9 +1,7 @@
-import Pergunta.Alternativa
-
 case class Tema(tema: String, perguntas: List[Pergunta]) {
 
 
-  def add_Pergunta(pergunta: Pergunta) = {
+  def add_Pergunta(pergunta: Pergunta): List[Pergunta] = {
     Tema.add_Pergunta(pergunta, this)
   }
 
@@ -13,17 +11,18 @@ case class Tema(tema: String, perguntas: List[Pergunta]) {
 }
 
 object Tema {
-  def add_Pergunta(pergunta: Pergunta, tema : Tema): Unit = {
-    def check_pergunta(perguntas: List[Pergunta]): Unit = {
+  def add_Pergunta(pergunta: Pergunta, tema : Tema): List[Pergunta] = {
+    def check_pergunta(perguntas: List[Pergunta]): List[Pergunta] = {
       perguntas match {
         case Nil => {
-          tema.perguntas :+ pergunta
           println("tema.perguntas :+ pergunta")
+          tema.perguntas :+ pergunta
         }
         case x :: t => {
-          if (x.pergunta == pergunta.pergunta)
+          if (x.pergunta == pergunta.pergunta) {
             println("Essa pergunta já existe!")
-          else
+            tema.perguntas
+          } else
             check_pergunta(t)
         }
       }
@@ -48,101 +47,7 @@ object Tema {
 
 }
 
-case class Pergunta(pergunta: String, alternativas: List[(String, Boolean)]) {
 
-  def receive_answer(alternativa: Alternativa): Boolean = Pergunta.receive_answer(alternativa, this.alternativas)
-
-  def print_Pergunta(): Unit ={Pergunta.print_Pergunta(this)}
-
-  def tip_Alternativas(): Unit = {Pergunta.tip_Alternativa(this)}
-}
-
-
-object Pergunta {
-  type Alternativa = (String, Boolean)
-
-  def receive_answer(alternativa: Alternativa, alternativas: List[Alternativa]): Boolean = {
-    alternativas match {
-      case Nil => false
-      case x :: t => {
-        if (x._1 == alternativa._1) {
-          if (x._2 == alternativa._2 && x._2 == true)
-            true
-          else
-            false
-        }
-        else
-          receive_answer(alternativa, t)
-      }
-    }
-  }
-
-  def print_Pergunta(pergunta: Pergunta): Unit = {
-    println(pergunta.pergunta)
-
-    def print_Alternativas(alternativas: List[(String, Boolean)]): Unit = {
-      println(alternativas.head._1)
-      alternativas match {
-        case x :: t => {
-          println(x._1)
-          if(t != Nil)
-            print_Alternativas(t)
-        }
-      }
-    }
-    print_Alternativas(pergunta.alternativas)
-  }
-
-  def tip_Alternativa(pergunta: Pergunta): Unit ={
-    def print_Alternativas(alternativa : List[(String,Boolean)], count : Int): Unit = {
-      pergunta.alternativas match {
-        case x :: t => {
-          if (x._2 == true) {
-            println(x._1)
-            if (count == 0)
-              print_Alternativas(t, count)
-          }else{
-              if(count == 0){
-                println(x._1)
-                print_Alternativas(t,1)
-              }
-              else
-                print_Alternativas(t,count)
-            }
-          }
-        }
-    }
-    print_Alternativas(pergunta.alternativas,0)
-    }
-
-
-  def main(args: Array[String]): Unit = {
-    val perguntas : List[Pergunta] = List()
-    val tema = "Esportes"
-    val esporte = Tema(tema, perguntas)
-    println("Foi criado o tema " + esporte.tema)
-    val pergunta_a: String = "Em que ano o Benfica foi fundado?"
-    val alternativas_a : List[(String,Boolean)] = List(("1906", false), ("1902", false),("1904", true), ("1870", false))
-    val a : Pergunta = Pergunta(pergunta_a,alternativas_a)
-    val pergunta_b: String = "Quem é o atual campeão europeu?"
-    val alternativas_b : List[(String,Boolean)] = List(("França", false), ("Portugal", true),("Espanha", false), ("Alemanha", false))
-    val b : Pergunta = Pergunta(pergunta_a,alternativas_a)
-    val pergunta_c: String = "Quem é o atual campeão da F1?"
-    val alternativas_c : List[(String,Boolean)] = List(("Hamilton", true), ("Leclerc", false),("Vettel", false), ("Verstappen", false))
-    val c : Pergunta = Pergunta(pergunta_c,alternativas_c)
-    esporte.add_Pergunta(a)
-    esporte.add_Pergunta(b)
-    esporte.add_Pergunta(c)
-    println("aaaaa")
-//    val pergunta_X : Pergunta = esporte.select_Pergunta()
-//    val pergunta_X = esporte.perguntas(1)
-    println("Pergunta_X:")
-    a.print_Pergunta()
-    println("Dica pergunta_X: ")
-//    a.tip_Alternativas()
-    println("a")
-  }
-}
 
 
 
