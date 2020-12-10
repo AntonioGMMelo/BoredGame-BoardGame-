c175e1b8accfb018882ead697031f7b75c47cae5
AntonioGMMelo <AntonioGMMelo@yahoo.com.br>
2020-12-10 21:48:36 +0000

    aaa

--- src/Board.scala
+++ src/Board.scala
@@ -14,71 +14,60 @@ class Board {
   type PlayersExtra=(String,(Int,Int),Boolean,Boolean,Boolean,Boolean,Boolean,Boolean,Boolean,Boolean)
   @FXML
   var circle1 :Circle =_
-  @FXML
-  var a:Button=_
+  var circle2 :Circle =_
+  var circle3 :Circle =_
+  var circle4 :Circle =_
+  var circle5 :Circle =_
+  var circle6 :Circle =_
+  var circle7 :Circle =_
+  var circle8 :Circle =_
+
   def display(players:List[PlayersExtra]) = {
     val fxmlLoader = new FXMLLoader(getClass.getResource("Board.fxml"))
     val mainViewRoot: Parent = fxmlLoader.load()
 
     //    val rect = new Rectangle()
     val popUp: Stage = new Stage()
-//    val circleex = new Circle()
-//    var circles: List[Circle]= List(circleex,circleex,circleex,circleex,circleex,circleex,circleex,circleex)
-//    val layout = new VBox(2)
-//    val layout2 = new AnchorPane()
-//    layout.setPrefHeight(1100)
-//    layout.setPrefWidth(1100)
-//    println(players)
-//  for(i<-0 to players.size-1){
-//    if(!(players(i).equals("NO"))) {
-//      val player = new Circle(players(i)._2._1, players(i)._2._2,10 - i)
-//      circles = circles.updated(i, player)
-//      if (players(i)._1.equals("BLACK")) {
-//        player.setFill(javafx.scene.paint.Color.BLACK)
-//      } else {
-//        if (players(i)._1.equals("WHITE")) {
-//          player.setFill(javafx.scene.paint.Color.WHITE)
-//        } else {
-//          if (players(i)._1.equals("BLUE")) {
-//            player.setFill(javafx.scene.paint.Color.BLUE)
-//          } else {
-//            if (players(i)._1.equals("CYAN")) {
-//              player.setFill(javafx.scene.paint.Color.CYAN)
-//            } else {
-//              if (players(i)._1.equals("RED")) {
-//                player.setFill(javafx.scene.paint.Color.RED)
-//              } else {
-//                if (players(i)._1.equals("GREEN")) {
-//                  player.setFill(javafx.scene.paint.Color.GREEN)
-//                } else {
-//                  if (players(i)._1.equals("MAGENTA")) {
-//                    player.setFill(javafx.scene.paint.Color.MAGENTA)
-//                  } else {
-//                    if (players(i)._1.equals("YELLOW")) {
-//                      player.setFill(javafx.scene.paint.Color.YELLOW)
-//                    }
-//                  }
-//                }
-//
-//              }
-//            }
-//          }
-//        }
-//      }
-//      layout.getChildren.add(circles(i))
-//    }
-//  }
-//  //setting up popUp's parameters and modality
-//    popUp.initModality(Modality.APPLICATION_MODAL)
-//    popUp.setTitle("Create Player")
-//    val s = new ScrollPa()
-//    s.setMin(0)
-//    s.setMax(1100)
-//    s.setValue(0)
-//    s.setOrientation(Orientation.VERTICAL)
-//    s.setUnitIncrement(200)
-//    s.setBlockIncrement(10)
-////    layout2.getChildren.addAll(s,layout)
+    var circles: List[Circle]= List(circle1,circle2,circle3,circle4,circle5,circle6,circle7,circle8)
+
+  for(i<-0 to players.size-1){
+    if(!(players(i).equals("NO"))) {
+      if (players(i)._1.equals("BLACK")) {
+        circles(i).setFill(javafx.scene.paint.Color.BLACK)
+      } else {
+        if (players(i)._1.equals("WHITE")) {
+          circles(i).setFill(javafx.scene.paint.Color.WHITE)
+        } else {
+          if (players(i)._1.equals("BLUE")) {
+            circles(i).setFill(javafx.scene.paint.Color.BLUE)
+          } else {
+            if (players(i)._1.equals("CYAN")) {
+              circles(i).setFill(javafx.scene.paint.Color.CYAN)
+            } else {
+              if (players(i)._1.equals("RED")) {
+                circles(i).setFill(javafx.scene.paint.Color.RED)
+              } else {
+                if (players(i)._1.equals("GREEN")) {
+                  circles(i).setFill(javafx.scene.paint.Color.GREEN)
+                } else {
+                  if (players(i)._1.equals("MAGENTA")) {
+                    circles(i).setFill(javafx.scene.paint.Color.MAGENTA)
+                  } else {
+                    if (players(i)._1.equals("YELLOW")) {
+                      circles(i).setFill(javafx.scene.paint.Color.YELLOW)
+                    }
+                  }
+                }
+
+              }
+            }
+          }
+        }
+      }
+      circles(i).setVisible(true)
+    }
+  }
+
       val scene: Scene= new Scene(mainViewRoot)
       popUp.setScene(scene)
       popUp.showAndWait()
