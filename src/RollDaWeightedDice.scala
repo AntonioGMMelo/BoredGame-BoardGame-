65808198cee264820c7495593988edb95b38b973
AntonioGMMelo <73307544+AntonioGMMelo@users.noreply.github.com>
2021-02-24 15:40:59 +0000

    fixed window size issue

--- src/RollDaWeightedDice.scala
+++ src/RollDaWeightedDice.scala
@@ -18,8 +18,8 @@ class RollDaWeightedDice {
     //setting up popUp's parameters and modality
     popUp.initModality(Modality.APPLICATION_MODAL)
     popUp.setTitle(label)
-    popUp.setMaxWidth(400)
-    popUp.setMaxHeight(300)
+    popUp.setMinWidth(400)
+    popUp.setMinHeight(300)
 
     //create button and button action
     val ok : Button = new Button("Roll the Weighted Dice")
